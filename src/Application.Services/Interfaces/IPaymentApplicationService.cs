004c6ff448721a20b6082967cc1340d3780992ae
Khauê Viana <khaue.viana@farfetch.com>
2022-06-23 00:04:00 +0100

    ongoing

---
+++ src/Application.Services/Interfaces/IPaymentApplicationService.cs
@@ -0,0 +1,11 @@
+﻿namespace PaymentGateway.Application.Services.Interfaces
+{
+    using PaymentGateway.Application.Dto.Payments;
+
+    public interface IPaymentApplicationService
+    {
+        Task<PaymentResponse> CreateAsync(PaymentRequest paymentRequest);
+
+        Task<PaymentResponse> GetAsync(Guid id);
+    }
+}
---
+++ src/Application.Services/PaymentApplicationService.cs
@@ -0,0 +1,20 @@
+﻿namespace PaymentGateway.Application.Services
+{
+    using PaymentGateway.Application.Dto.Payments;
+    using PaymentGateway.Application.Services.Interfaces;
+    using System;
+    using System.Threading.Tasks;
+
+    public class PaymentApplicationService : IPaymentApplicationService
+    {
+        public Task<PaymentResponse> CreateAsync(PaymentRequest paymentRequest)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<PaymentResponse> GetAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
